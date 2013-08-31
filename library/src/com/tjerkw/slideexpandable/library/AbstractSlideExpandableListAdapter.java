40d8c991f2a073cbef4044dd88788ef504085ce0
Dino Kovač <dino.kovac.c3@gmail.com>
2013-08-31 15:29:34 +0200

    Added OnItemExpandCollapseListener code to AbstractSlideExpandableListAdapter

--- library/src/com/tjerkw/slideexpandable/library/AbstractSlideExpandableListAdapter.java
+++ library/src/com/tjerkw/slideexpandable/library/AbstractSlideExpandableListAdapter.java
@@ -73,6 +73,63 @@ public abstract class AbstractSlideExpandableListAdapter extends WrapperListAdap
 		}
 	}
 
+	private OnItemExpandCollapseListener expandCollapseListener;
+
+	/**
+	 * Sets a listener which gets call on item expand or collapse
+	 * 
+	 * @param listener
+	 *            the listener which will be called when an item is expanded or
+	 *            collapsed
+	 */
+	public void setItemExpandCollapseListener(
+			OnItemExpandCollapseListener listener) {
+		expandCollapseListener = listener;
+	}
+
+	public void removeItemExpandCollapseListener() {
+		expandCollapseListener = null;
+	}
+
+	/**
+	 * Interface for callback to be invoked whenever an item is expanded or
+	 * collapsed in the list view.
+	 */
+	public interface OnItemExpandCollapseListener {
+		/**
+		 * Called when an item is expanded.
+		 * 
+		 * @param itemView
+		 *            the view of the list item
+		 * @param position
+		 *            the position in the list view
+		 */
+		public void onExpand(View itemView, int position);
+
+		/**
+		 * Called when an item is collapsed.
+		 * 
+		 * @param itemView
+		 *            the view of the list item
+		 * @param position
+		 *            the position in the list view
+		 */
+		public void onCollapse(View itemView, int position);
+
+	}
+
+	private void notifiyExpandCollapseListener(int type, View view, int position) {
+		if (expandCollapseListener != null) {
+			if (type == ExpandCollapseAnimation.EXPAND) {
+				expandCollapseListener.onExpand(view, position);
+			} else if (type == ExpandCollapseAnimation.COLLAPSE) {
+				expandCollapseListener.onCollapse(view, position);
+			}
+		}
+
+	}
+
+
 	@Override
 	public View getView(int position, View view, ViewGroup viewGroup) {
 		this.parent = viewGroup;
@@ -220,6 +277,9 @@ public abstract class AbstractSlideExpandableListAdapter extends WrapperListAdap
 						if (lastOpenPosition != -1 && lastOpenPosition != position) {
 							if (lastOpen != null) {
 								animateView(lastOpen, ExpandCollapseAnimation.COLLAPSE);
+								notifiyExpandCollapseListener(
+										ExpandCollapseAnimation.COLLAPSE,
+										lastOpen, lastOpenPosition);
 							}
 							openItems.set(lastOpenPosition, false);
 						}
@@ -229,6 +289,7 @@ public abstract class AbstractSlideExpandableListAdapter extends WrapperListAdap
 						lastOpenPosition = -1;
 					}
 					animateView(target, type);
+					notifiyExpandCollapseListener(type, target, position);
 				}
 			}
 		});
